b817260c08f11be1e3a5389c9f049ab78463fe59
Alicia <112453488+wapisai@users.noreply.github.com>
2024-03-26 13:41:45 +0800

    Update src/main/java/seedu/realodex/logic/parser/DeleteCommandParser.java
    Co-authored-by: Udhaya <39399745+UdhayaShan1@users.noreply.github.com>

--- src/main/java/seedu/realodex/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/realodex/logic/parser/DeleteCommandParser.java
@@ -12,8 +12,8 @@ import seedu.realodex.model.person.Name;
  */
 public class DeleteCommandParser implements Parser<DeleteCommand> {
 
-    public static final String INDEX_AND_NAME_PROVIDED = "Please provide either an index or a name, not both.";
-    public static final String NO_FIELDS_PROVIDED = "Please provide either an index or a name.";
+    public static final String MESSAGE_INDEX_AND_NAME_PROVIDED = "Please provide either an index or a name, not both.";
+    public static final String MESSAGE_NO_FIELDS_PROVIDED = "Please provide either an index or a name.";
 
     /**
      * Parses the given {@code String} of arguments in the context of the DeleteCommand
