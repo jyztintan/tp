86729ffbcb993a332eec4a695b1a7728566ddbaf
wapisai <e0968302@u.nus.edu>
2024-03-30 02:12:49 +0800

    Add HousingType class

---
+++ src/main/java/seedu/realodex/model/person/HousingType.java
@@ -0,0 +1,65 @@
+package seedu.realodex.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.realodex.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Tag in the realodex.
+ * Guarantees: immutable; name is valid as declared in {@link #isValidHousingType(String)}
+ */
+public class HousingType {
+
+    public static final String MESSAGE_CONSTRAINTS = "Housing type should be either 'HDB', 'CONDOMINIUM', " +
+            "'LANDED_PROPERTY' or 'GOOD_CLASS_BUNGALOW'";
+    public static final String VALIDATION_REGEX = "\\p{Alnum}+";
+    private House housingType;
+    public enum House {
+        HDB, CONDOMINIUM, LANDED_PROPERTY, GOOD_CLASS_BUNGALOW
+    }
+
+    public HousingType(String housingType) {
+        requireNonNull(housingType);
+        checkArgument(isValidHousingType(housingType), MESSAGE_CONSTRAINTS);
+        this.housingType = House.valueOf(housingType.toUpperCase());
+    }
+    
+    /**
+     * Returns true if a given string is a valid tag name.
+     */
+    public static boolean isValidHousingType(String test) {
+        try {
+            House.valueOf(test.toUpperCase().replace(" ", "_"));
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof House)) {
+            return false;
+        }
+
+        HousingType otherHouse = (HousingType) other;
+        return this.housingType.equals(otherHouse.housingType);
+    }
+
+    @Override
+    public int hashCode() {
+        return housingType.hashCode();
+    }
+
+    /**
+     * Format state as text for viewing.
+     */
+    public String toString() {
+        return '[' + housingType.toString() + ']';
+    }
+
+}
