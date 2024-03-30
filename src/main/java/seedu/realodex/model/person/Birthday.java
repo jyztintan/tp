850c4f8c64e942a21d1bf0652d0645955ca294e4
appleraincoat <james20111951@gmail.com>
2024-03-30 17:39:59 +0800

    Fix minor checkstyle issue

--- src/main/java/seedu/realodex/model/person/Birthday.java
+++ src/main/java/seedu/realodex/model/person/Birthday.java
@@ -3,7 +3,6 @@ package seedu.realodex.model.person;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Optional;
