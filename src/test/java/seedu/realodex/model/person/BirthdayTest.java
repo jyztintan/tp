bdd844bc4d3cefac5ba6e9d52471760f85cf85ef
appleraincoat <james20111951@gmail.com>
2024-03-30 17:20:07 +0800

    Fix minor checkstyle issue

--- src/test/java/seedu/realodex/model/person/BirthdayTest.java
+++ src/test/java/seedu/realodex/model/person/BirthdayTest.java
@@ -8,7 +8,6 @@ import static seedu.realodex.testutil.Assert.assertThrows;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-
 import org.junit.jupiter.api.Test;
 
 public class BirthdayTest {
