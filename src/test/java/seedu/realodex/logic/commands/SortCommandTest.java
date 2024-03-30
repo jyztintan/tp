9c2edebc6a13acad50aa06daf468670151146955
UdhayaShan1 <udhaya.shan1@gmail.com>
2024-03-30 16:01:38 +0800

    Add SortCommand tests

---
+++ src/test/java/seedu/realodex/logic/commands/SortCommandTest.java
@@ -0,0 +1,62 @@
+package seedu.realodex.logic.commands;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import seedu.realodex.logic.commands.SortCommand;
+import seedu.realodex.logic.commands.exceptions.CommandException;
+import seedu.realodex.model.Model;
+import seedu.realodex.model.ModelManager;
+import seedu.realodex.model.Realodex;
+import seedu.realodex.model.UserPrefs;
+import seedu.realodex.model.person.Person;
+import seedu.realodex.model.person.sorting.BirthdayComparator;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.realodex.testutil.TypicalPersons.getTypicalRealodex;
+
+public class SortCommandTest {
+    private final Model model = new ModelManager(getTypicalRealodex(), new UserPrefs());
+    @Test
+    public void execute_sortsListByBirthday_success() {
+        // Create a list of persons with birthdays in random order
+        Realodex realodex = (Realodex) model.getRealodex();
+
+        // Execute the sort command
+        SortCommand sortCommand = new SortCommand();
+        try {
+            sortCommand.execute(model);
+        } catch (CommandException e) {
+            Assertions.fail();
+        }
+
+        // Verify that the list is sorted by birthday
+        List<Person> sortedList = realodex.getCopyOfInternalListOfUniquePersonsList();
+        assertTrue(isSortedByBirthday(sortedList));
+    }
+
+    @Test
+    public void execute_returnsSuccessMessage() {
+        // Execute the sort command
+        SortCommand sortCommand = new SortCommand();
+        try {
+            CommandResult result = sortCommand.execute(model);
+            assertEquals(SortCommand.MESSAGE_SUCCESS, result.getFeedbackToUser());
+        } catch (CommandException e) {
+            Assertions.fail();
+        }
+    }
+
+    // Helper method to check if the list is sorted by birthday
+    private boolean isSortedByBirthday(List<Person> persons) {
+        for (int i = 1; i < persons.size(); i++) {
+            Person previousPerson = persons.get(i - 1);
+            Person currentPerson = persons.get(i);
+            if (new BirthdayComparator().compare(previousPerson, currentPerson) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
