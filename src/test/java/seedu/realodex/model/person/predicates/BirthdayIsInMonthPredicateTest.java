12085c5ad070f939d120e7e404e3a36b62cf3ca3
Allysa Tan <allysa@u.nus.edu>
2024-03-31 11:10:55 +0800

    Add tests

--- src/test/java/seedu/realodex/model/person/predicates/BirthdayIsInMonthPredicateTest.java
+++ src/test/java/seedu/realodex/model/person/predicates/BirthdayIsInMonthPredicateTest.java
@@ -3,6 +3,7 @@ package seedu.realodex.model.person.predicates;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.realodex.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -42,8 +43,8 @@ class BirthdayIsInMonthPredicateTest {
         assertTrue(predicate.test(new PersonBuilder().withBirthday("1Jan2001").build()));
 
         // Keyphrase is valid month with 4 letters
-        predicate = new BirthdayIsInMonthPredicate("Sept");
-        assertTrue(predicate.test(new PersonBuilder().withBirthday("11Sep2001").build()));
+        predicate = new BirthdayIsInMonthPredicate("Oct");
+        assertTrue(predicate.test(new PersonBuilder().withBirthday("11Oct2001").build()));
 
         // Keyphrase is february and Birthday in leap year
         predicate = new BirthdayIsInMonthPredicate("Feb");
@@ -55,10 +56,46 @@ class BirthdayIsInMonthPredicateTest {
     }
 
     @Test
+    public void test_birthdayIsNotInMonth_returnsFrue() {
+        // Keyphrase is valid month with 3 letters
+        BirthdayIsInMonthPredicate predicate = new BirthdayIsInMonthPredicate("Jun");
+        assertFalse(predicate.test(new PersonBuilder().withBirthday("1Jan2001").build()));
+
+        // Keyphrase is valid month with full month
+        predicate = new BirthdayIsInMonthPredicate("September");
+        assertFalse(predicate.test(new PersonBuilder().withBirthday("11October2001").build()));
+
+        // Mixed-case keyphrase
+        predicate = new BirthdayIsInMonthPredicate("jAn");
+        assertFalse(predicate.test(new PersonBuilder().withBirthday("1Feb2001").build()));
+    }
+
+    @Test
+    public void test_birthdayNotSpecified_returnsFalse() {
+        BirthdayIsInMonthPredicate predicate = new BirthdayIsInMonthPredicate("Jun");
+        assertFalse(predicate.test(new PersonBuilder().build()));
+
+        predicate = new BirthdayIsInMonthPredicate("October");
+        assertFalse(predicate.test(new PersonBuilder().withBirthday("").build()));
+    }
+    @Test
     void toStringMethod() {
         String keyphrase = "Aug";
         BirthdayIsInMonthPredicate predicate = new BirthdayIsInMonthPredicate(keyphrase);
         String expected = BirthdayIsInMonthPredicate.class.getCanonicalName() + "{month=August}";
         assertEquals(expected, predicate.toString());
+    }
+
+    // the following tests are to meet CodeCov Requirements, will not be reached under normal use
+    @Test
+    void toStringMethod_noBirthMonthSpecified() {
+        // empty String input
+        String keyphraseEmptyString = "";
+        assertThrows(AssertionError.class, null ,() -> new BirthdayIsInMonthPredicate(keyphraseEmptyString) );
+
+        // invalid String input
+        String keyphraseInvalidString = "sndjkfnksdnf";
+        assertThrows(AssertionError.class, null ,() -> new BirthdayIsInMonthPredicate(keyphraseInvalidString) );
+
     }
 }
