32b7cf45dd20afb2d1c361974fcbe15aea6c066b
UdhayaShan1 <udhaya.shan1@gmail.com>
2024-04-02 17:11:19 +0800

    Edit EditCommandParser to build up the error message

--- src/main/java/seedu/realodex/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/realodex/logic/parser/EditCommandParser.java
@@ -12,15 +12,29 @@ import static seedu.realodex.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.realodex.logic.parser.CliSyntax.PREFIX_REMARK;
 import static seedu.realodex.logic.parser.CliSyntax.PREFIX_TAG;
 
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
 import seedu.realodex.commons.core.index.Index;
 import seedu.realodex.logic.commands.EditCommand;
 import seedu.realodex.logic.commands.EditCommand.EditPersonDescriptor;
 import seedu.realodex.logic.parser.exceptions.ParseException;
-
+import seedu.realodex.model.person.Address;
+import seedu.realodex.model.person.Birthday;
+import seedu.realodex.model.person.Email;
+import seedu.realodex.model.person.Family;
+import seedu.realodex.model.person.Income;
+import seedu.realodex.model.person.Name;
+import seedu.realodex.model.person.Phone;
+import seedu.realodex.model.tag.Tag;
+//@@author UdhayaShan1
 /**
  * Parses input arguments and creates a new EditCommand object
  */
 public class EditCommandParser implements Parser<EditCommand> {
+
+    private static final String MESSAGE_ERROR_PARSING_TAGS = "Error parsing tags: " + Tag.MESSAGE_CONSTRAINTS;
 
     /**
      * Parses the given {@code String} of arguments in the context of the EditCommand
@@ -29,54 +43,70 @@ public class EditCommandParser implements Parser<EditCommand> {
      */
     public EditCommand parse(String args) throws ParseException {
         requireNonNull(args);
-        ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_INCOME, PREFIX_EMAIL, PREFIX_ADDRESS,
-                        PREFIX_FAMILY, PREFIX_TAG, PREFIX_REMARK, PREFIX_BIRTHDAY);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_INCOME,
+                                                                PREFIX_EMAIL, PREFIX_ADDRESS,
+                                   PREFIX_FAMILY, PREFIX_TAG, PREFIX_REMARK, PREFIX_BIRTHDAY);
 
-        Index index;
+        Index index = parseIndex(argMultimap.getPreamble());
 
+        EditPersonDescriptor editPersonDescriptor = parseEditPersonDescriptor(argMultimap);
+
+        return new EditCommand(index, editPersonDescriptor);
+    }
+
+    private Index parseIndex(String preamble) throws ParseException {
         try {
-            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+            return ParserUtil.parseIndex(preamble);
         } catch (ParseException pe) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditCommand.MESSAGE_USAGE), pe);
         }
+    }
 
-        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_PHONE, PREFIX_INCOME, PREFIX_EMAIL, PREFIX_ADDRESS,
-                PREFIX_FAMILY, PREFIX_REMARK, PREFIX_BIRTHDAY);
 
+    private EditPersonDescriptor parseEditPersonDescriptor(ArgumentMultimap argMultimap) throws ParseException {
         EditPersonDescriptor editPersonDescriptor = new EditPersonDescriptor();
+        StringBuilder errorMessageBuilder = new StringBuilder();
 
-        if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
-            editPersonDescriptor.setName(ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get()));
+        parseAndSetField(argMultimap, PREFIX_NAME, editPersonDescriptor::setName, ParserUtil::parseNameReturnStored, errorMessageBuilder, "name");
+        parseAndSetField(argMultimap, PREFIX_PHONE, editPersonDescriptor::setPhone, ParserUtil::parsePhoneReturnStored, errorMessageBuilder, "phone");
+        parseAndSetField(argMultimap, PREFIX_INCOME, editPersonDescriptor::setIncome, ParserUtil::parseIncomeReturnStored, errorMessageBuilder, "income");
+        parseAndSetField(argMultimap, PREFIX_EMAIL, editPersonDescriptor::setEmail, ParserUtil::parseEmailReturnStored, errorMessageBuilder, "email");
+        parseAndSetField(argMultimap, PREFIX_ADDRESS, editPersonDescriptor::setAddress, ParserUtil::parseAddressReturnStored, errorMessageBuilder, "address");
+        parseAndSetField(argMultimap, PREFIX_FAMILY, editPersonDescriptor::setFamily, ParserUtil::parseFamilyReturnStored, errorMessageBuilder, "family");
+        parseAndSetField(argMultimap, PREFIX_BIRTHDAY, editPersonDescriptor::setBirthday, ParserUtil::parseBirthdayReturnStored, errorMessageBuilder, "birthday");
+
+        if (argMultimap.containsPrefix(PREFIX_TAG)) {
+            try {
+                Set<Tag> parsedTagsForEdit = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
+                editPersonDescriptor.setTags(parsedTagsForEdit);
+            } catch (ParseException exception) {
+                errorMessageBuilder.append(MESSAGE_ERROR_PARSING_TAGS);
+            }
         }
-        if (argMultimap.getValue(PREFIX_PHONE).isPresent()) {
-            editPersonDescriptor.setPhone(ParserUtil.parsePhone(argMultimap.getValue(PREFIX_PHONE).get()));
-        }
-        if (argMultimap.getValue(PREFIX_INCOME).isPresent()) {
-            editPersonDescriptor.setIncome(ParserUtil.parseIncome(argMultimap.getValue(PREFIX_INCOME).get()));
-        }
-        if (argMultimap.getValue(PREFIX_EMAIL).isPresent()) {
-            editPersonDescriptor.setEmail(ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL).get()));
-        }
-        if (argMultimap.getValue(PREFIX_ADDRESS).isPresent()) {
-            editPersonDescriptor.setAddress(ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get()));
-        }
-        if (argMultimap.getValue(PREFIX_FAMILY).isPresent()) {
-            editPersonDescriptor.setFamily(ParserUtil.parseFamily(argMultimap.getValue(PREFIX_FAMILY).get()));
-        }
-        if (argMultimap.getValue(PREFIX_TAG).isPresent()) {
-            editPersonDescriptor.setTags(ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG)));
-        }
+
         if (argMultimap.containsPrefix(PREFIX_REMARK)) {
             editPersonDescriptor.setRemark(ParserUtil.parseRemark(argMultimap.getValue(PREFIX_REMARK).get()));
         }
-        if (argMultimap.containsPrefix(PREFIX_BIRTHDAY)) {
-            editPersonDescriptor.setBirthday(ParserUtil.parseBirthday(argMultimap.getValue(PREFIX_BIRTHDAY).get()));
+
+        if (errorMessageBuilder.length() > 0) {
+            throw new ParseException(errorMessageBuilder.toString());
         }
+
         if (!editPersonDescriptor.isAnyFieldEdited()) {
             throw new ParseException(EditCommand.MESSAGE_NOT_EDITED);
         }
 
-        return new EditCommand(index, editPersonDescriptor);
+        return editPersonDescriptor;
+    }
+
+    private <T> void parseAndSetField(ArgumentMultimap argMultimap, Prefix prefix, Consumer<T> setter,
+                                      Function<String, ParserUtilResult<T>> parserFunction,
+                                      StringBuilder errorMessageBuilder, String fieldName) {
+        if (argMultimap.getValue(prefix).isPresent()) {
+            ParserUtilResult<T> result = parserFunction.apply(argMultimap.getValue(prefix).get());
+            result.buildErrorMessage(errorMessageBuilder, fieldName);
+            setter.accept(result.returnStoredResult());
+        }
     }
 }
+//@@author
