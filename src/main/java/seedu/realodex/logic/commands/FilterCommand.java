cb7e701029b3d56db11921c113f569272db67343
jyztintan <justintanwk2001@gmail.com>
2024-04-03 09:21:50 +0800

    Update Filter command descriptions
    Include housing type for MESSAGE_USAGE and
    MESSAGE_FILTER_HELP

--- src/main/java/seedu/realodex/logic/commands/FilterCommand.java
+++ src/main/java/seedu/realodex/logic/commands/FilterCommand.java
@@ -1,6 +1,7 @@
 package seedu.realodex.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.realodex.logic.parser.CliSyntax.PREFIX_HOUSINGTYPE;
 import static seedu.realodex.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.realodex.logic.parser.CliSyntax.PREFIX_REMARK;
 import static seedu.realodex.logic.parser.CliSyntax.PREFIX_TAG;
@@ -22,22 +23,23 @@ public class FilterCommand extends Command {
     public static final String COMMAND_WORD = "filter";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Filters all clients by specified field (name, remark, tag) "
+            + ": Filters all clients by specified field (name, remark, tag, housing type) "
             + "with the specified keyphrase (non-empty, case-insensitive) "
             + "and displays them as a list with index numbers.\n"
             + "Note that although the fields are listed as optional, ONE field must strictly be present.\n"
             + "Parameters: "
             + "[" + PREFIX_NAME + "KEYPHRASE] "
             + "[" + PREFIX_REMARK + "REMARK]"
-            + "[" + PREFIX_TAG + "TAG]\n"
+            + "[" + PREFIX_TAG + "TAG]"
+            + "[" + PREFIX_HOUSINGTYPE + "HOUSING_TYPE]\n"
             + "Example: " + COMMAND_WORD + " " + PREFIX_NAME + "alice tan";
 
     public static final String MESSAGE_FILTER_HELP = "Filter Command: "
-            + "Filters clients by ONE specified field (name, remark, tag)"
+            + "Filters clients by ONE specified field (name, remark, tag, housing type)"
             + "with the specified keyphrase (non-empty, case-insensitive) "
             + "and displays them as a list with index numbers.\n"
-            + "Format: filter [n/KEYPHRASE] [r/KEYPHRASE] [t/TAG]\n"
-            + "Example: filter n/Jus\n";
+            + "Format: filter [n/KEYPHRASE] [r/KEYPHRASE] [t/TAG] [h/HOUSING_TYPE]\n"
+            + "Example: " + "filter n/alice tan";
 
     public static final String MESSAGE_FILTER_CONFLICT = "Filter command can only filter by one field.\n";
 
